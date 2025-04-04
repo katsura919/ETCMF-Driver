bc0e6cbddf7cba7907d2ad7945db72cd31428387
Jan Paul <katsuragik919@gmail.com>
2025-04-04 21:03:56 +0800

    added middleware

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const { registerDriver, loginDriver } = require('../controllers/authController');
-
+const authMiddleware = require('../middleware/authMiddleware');
 require('../api-docs/api/authSwagger');  
 
 const router = express.Router();
 
 // Register route
-router.post('/register', registerDriver);
+router.post('/register', authMiddleware, registerDriver);
 
 // Login route
-router.post('/login', loginDriver);
+router.post('/login', authMiddleware, loginDriver);
 
 module.exports = router;
